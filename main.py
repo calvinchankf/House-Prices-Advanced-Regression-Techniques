9bc63d4ccc86b1aea77e981e143162d34307d8c0
calvinchankf <chan9118kin@gmail.com>
2018-10-25 23:57:15 +0800

    do it in OOP, easier to change

--- main.py
+++ main.py
@@ -5,64 +5,80 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn import linear_model
 
-train = pd.read_csv('./data/train.csv')
-
 # TODO:
 # feature encoding: WIP
 # remove outliers: TBD
 
-# transform categorical data to numeric data, i.e. one-hot/feature-encoding
-categoricals = train.select_dtypes(exclude=[np.number])
-categorical_columns = categoricals.columns.tolist()
-abc = pd.get_dummies(train, columns=categorical_columns)
-print("🤡", abc.columns.tolist())
-# only select the numeric type features (remove the non-numeric)
-data = train.select_dtypes(include=[np.number]).interpolate().dropna()
+class HousePrices():
 
-# split X and Y
-y = np.log(data.SalePrice)
-X = data.drop(['SalePrice', 'Id'], axis=1) # remove SalePrice and Id from column(axis=1)
-print("😅", len(X.columns.tolist()))
+  def __init__(self):
+    self.model = None
+    self.X_test = None
+    self.Y_test = None
 
-# split train and test
-# random_state: guarantee data are always splited into the same train & test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=.33)
-lr = linear_model.LinearRegression()
-model = lr.fit(X_train, y_train)
-print ("R^2 is: \n", model.score(X_test, y_test))
+  def trainModel(self):
+    train = pd.read_csv('./data/train.csv')
 
-# cross validation: use the model to predict the result of X_test
-predictions = model.predict(X_test)
-print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+    train = self.extractNumeric(train)
 
-actual_values = y_test
-plt.scatter(predictions, actual_values, alpha=.75, color='b') #alpha helps to show overlapping data
-plt.xlabel('Predicted Price')
-plt.ylabel('Actual Price')
-plt.title('Linear Regression Model')
-# plt.show()
+    Y = np.log(train.SalePrice)
+    X = train.drop(['SalePrice', 'Id'], axis=1)
 
-test = pd.read_csv('./data/test.csv')
+    X_train, self.X_test, Y_train, self.Y_test = train_test_split(X, Y, random_state=42, test_size=.33)
+    lr = linear_model.LinearRegression()
+    self.model = lr.fit(X_train, Y_train)
+    print ("R^2 is: \n", self.model.score(self.X_test, self.Y_test))
 
-submission = pd.DataFrame()
-submission['Id'] = test.Id
+    return
 
-categoricals = test.select_dtypes(exclude=[np.number])
-categorical_columns = categoricals.columns.tolist()
-abc = pd.get_dummies(test, columns=categorical_columns)
-print("🤡", abc.columns.tolist())
-feats = test.select_dtypes(include=[np.number]).interpolate().dropna()
-feats = feats.drop(['Id'], axis=1)
-print("😅", len(feats.columns.tolist()))
+  # input:  training set
+  # output: training set only with numeric values
+  # pure function
+  def extractNumeric(self, data):
+    return data.select_dtypes(include=[np.number]).interpolate().dropna()
 
-predictions = model.predict(feats)
+  # input:  training set
+  # output: feature encoded training set
+  # pure function
+  # problem: inconsistent columns
+  def oneHotEncode(self, data):
+    categoricals = data.select_dtypes(exclude=[np.number])
+    categorical_columns = categoricals.columns.tolist()
+    result = pd.get_dummies(train, columns=categorical_columns)
+    return result
 
-final_predictions = np.exp(predictions)
+  # predict the test data by train_test_split
+  def crossValidate(self):
+    if self.X_test is None or self.Y_test is None:
+      return
+    predictions = self.model.predict(self.X_test)
+    print ('RMSE is: \n', mean_squared_error(self.Y_test, predictions))
+    # plot it
+    plt.scatter(predictions, self.Y_test, alpha=.75, color='b') #alpha helps to show overlapping data
+    plt.xlabel('Predicted Price')
+    plt.ylabel('Actual Price')
+    plt.title('Linear Regression Model')
+    plt.show()
+    return
+  
+  # predict data for submission
+  def predict(self):
+    data = pd.read_csv('./data/test.csv')
 
-print ("Original predictions are: \n", predictions[:5], "\n")
-print ("Final predictions are: \n", final_predictions[:5])
+    submission = pd.DataFrame()
+    submission['Id'] = data.Id
 
-submission['SalePrice'] = final_predictions
-submission.head()
+    data = self.extractNumeric(data)
+    data = data.drop(['Id'], axis=1)
 
-submission.to_csv('./submission2.csv', index=False)
+    predictions = self.model.predict(data)
+    final_predictions = np.exp(predictions)
+    print ("Final predictions are: \n", final_predictions[:5])
+
+    submission['SalePrice'] = final_predictions
+    submission.to_csv('./submission2.csv', index=False)
+    return
+
+hp = HousePrices()
+hp.trainModel()
+hp.crossValidate()
