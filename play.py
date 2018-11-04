54e75bcb4f4464206a07aa1f08fc8a9b7b581fef
calvinchankf <chan9118kin@gmail.com>
2018-11-04 16:27:55 +0800

    remove .vscode

--- play.py
+++ play.py
@@ -1,81 +1,58 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
 
-# step 1: visualization
-# plot any one of the params(x1) against the real data(y)
-train = pd.read_csv('./data/train.csv')
-x = train['LotArea']
-y = train['SalePrice']
+df_train = pd.read_csv('./data/train.csv')
 
-# describe
-sp = train['SalePrice']
-print(sp.describe())
-print ("Skew is:", sp.skew())
+# box plot overallqual/saleprice
+var = 'OverallQual'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000)
+plt.show()
 
-# extract the numeric factors
-numeric_features = train.select_dtypes(include=[np.number])
-print(numeric_features.dtypes)
+# correlation matrix
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True)
+plt.show()
 
-# see correlation
-corr = numeric_features.corr()
-print (corr['SalePrice'].sort_values(ascending=False)[:5], '\n')
-print (corr['SalePrice'].sort_values(ascending=False)[-5:])
+# saleprice correlation matrix
+k = 10  # number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={
+    'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
 
-# discrete value
-print("Unique OverallQual", train.OverallQual.unique())
-print("Unique MiscFeature", train.MiscFeature.unique())
+# missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()
+           ).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+print(missing_data.head(20))
+# it seems like PoolQC, MiscFeature, Alley are not important, i.e. outliers
 
-# take a look at the median of y for each OverallQual discrete value
-quality_pivot = train.pivot_table(index='OverallQual', values='SalePrice', aggfunc=np.median)
-print(quality_pivot)
-# plot it
-quality_pivot.plot(kind='bar', color='blue')
-plt.xlabel('Overall Quality')
-plt.ylabel('Median Sale Price')
-plt.xticks(rotation=0)
-# plt.show()
+# standardizing data
+saleprice_scaled = StandardScaler().fit_transform(
+    df_train['SalePrice'][:, np.newaxis])
+low_range = saleprice_scaled[saleprice_scaled[:, 0].argsort()][:10]
+high_range = saleprice_scaled[saleprice_scaled[:, 0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
 
-# check correlation between GrLivArea and Sale Price
-plt.scatter(x=train['GrLivArea'], y=np.log(sp))
-plt.ylabel('Sale Price')
-plt.xlabel('Above grade (ground) living area square feet')
-# plt.show()
-
-# check correlation between GarageArea and Sale Price
-plt.scatter(x=train['GarageArea'], y=np.log(sp))
-plt.ylabel('Sale Price')
-plt.xlabel('Garage Area')
-# plt.show()
-
-# remove outliers in the stupidest way 
-temp = train[train['GarageArea'] < 1200]
-plt.scatter(x=temp['GarageArea'], y=np.log(temp.SalePrice))
-plt.xlim(-200,1600) # This forces the same scale as before
-plt.ylabel('Sale Price')
-plt.xlabel('Garage Area')
-# plt.show()
-
-# checkout null
-nulls = pd.DataFrame(train.isnull().sum().sort_values(ascending=False)[:25])
-nulls.columns = ['Null Count']
-nulls.index.name = 'Feature'
-print(nulls)
-
-# checkout non-numeric data
-categoricals = train.select_dtypes(exclude=[np.number])
-print("🙄", categoricals.describe())
-print("🤔", categoricals.columns.tolist())
-categorical_columns = categoricals.columns.tolist()
-# print("🏞", categoricals.iloc[:1,:].values.tolist())
-
-# feature-encode the value of "Street"
-# Pave => 1
-# Grav => 0
-# train['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
-# print (train.enc_street.value_counts())
-
-# feature-encode the value of "MSZoning"
-train = pd.get_dummies(train, columns=categorical_columns, drop_first=True)
-print(train)
-# print(train['enc_street'])
+# bivariate analysis saleprice/grlivarea
+# finding outliers from GrLivArea
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+plt.show()
